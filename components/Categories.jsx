fc77b09511b7163b5d56b26f6dee5ed4d789d716
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2022-05-17 15:14:26 +0530

    Header categories finished

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -10,6 +10,7 @@ const Categories = () => {
     getCategories()
     .then((newCategories)=> setCategories(newCategories))
   }, []);
+  
   return (
     <div className="bg-white shadow-lg rounded-lg p-8 mb-8 pb-12">
       <h3 className="text-xl mb-8 font-semibold border-b pb-4">
