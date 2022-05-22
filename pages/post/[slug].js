f38aa61e7d2182c36fdf4b92680a0921636d6156
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2022-05-22 22:08:38 +0530

    Complete project and deploy

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,11 +1,17 @@
 import React from 'react'
+import { useRouter } from 'next/router';
 
 import {getPosts, getPostDetails} from '../../services';
 
-import {PostDetail,Categories,PostWidget,Author,Comments,CommentsForm} from '../../components';
+import {PostDetail,Categories,PostWidget,Author,Comments,CommentsForm, Loader} from '../../components';
 
 const PostDetails = ({post}) => {
-    console.log(post);
+
+   const router = useRouter();
+
+   if(router.isFallback){
+       return <Loader/>
+   }
   return (
     <div className="container mx-auto px-10 mb-8">
         <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
@@ -42,6 +48,6 @@ export async function getStaticProps({params}) {
 
       return{
           paths: posts.map(({node:{ slug }})=>({ params:{slug } })),
-          fallback:false,
+          fallback:true,
       }
   }
